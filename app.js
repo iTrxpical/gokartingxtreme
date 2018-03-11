f52da7899ce5198347a1260b059a5ed033c00f47
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-11 14:21:45 +0000

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ client.on('message', message => {
 
     let boe = message.guild.roles.find(r => r.name === "Board of Exectives") || message.guild.roles.find(r => r.name === "Chief Executive Officer");
     if (!message.author.hasRole(boe)) return;
-    if (!message.channel.id === '422091691604967425') message.delete(10000) return;
+    if (!message.channel.id === '422091691604967425') message.delete(10000)
 
 })
    client.on('message', message => {
