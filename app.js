80a1ee703f002f865ca955ff27ce4797938b3559
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-11 14:37:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ client.on('message', message => {
             console.log(`${message.author.username} ran the command: ${cmd} sucessfully!`);
 
         }
-    }
+    })
 
 
 client.login(process.env.TOKEN);
