8685347bb4633ffe8beb669f13d9ae2c660a1597
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-13 16:43:22 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,12 @@
 exports.run = (Discord, client, message, args) => {
 
+    message.delete(10)
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     var embedping = new Discord.RichEmbed()
         .setColor(randomColor)
         .addField(":ping_pong: Pongy!", new Date().getTime() - message.createdTimestamp + " ms")
     message.channel.sendEmbed(embedping);
+    .then(message => {
     message.delete(10000)
+  })
 }
