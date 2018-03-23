484a3d870a85622523e803897a243493aef2966f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-23 19:45:37 +0000

    Update privatesession.js

--- commands/privatesession.js
+++ commands/privatesession.js
@@ -4,7 +4,7 @@ exports.run = (Discord, client, message, args) => {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     var embedprivatesession = new Discord.RichEmbed()
         .setColor(randomColor)
-        .setTitle(":wave: Private Booking")
-        .addField(":How to create a private booking:", new Date().getTime() - message.createdTimestamp + " ms")
+        .setTitle(":book: Private Booking")
+        .addField("How to create a private booking:", "I will add this at some point")
     message.channel.sendEmbed(embedprivatesession);
 }
