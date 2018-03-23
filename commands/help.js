f6ca2549baf3a9927dae797bc04b7b640dc8a368
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-23 18:58:05 +0000

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,13 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+exports.run = (Discord, client, message, args) => {
+
+    var embedinfo = new Discord.RichEmbed()
+        .setFooter("© Go-Karting Xtreme | Made by TheReal (Catty)")
+        .setColor(randomColor)
+        .setTitle(":exclamation: Help")
+        .setDescription("Please run =info for help.")
+    message.channel.sendEmbed(embedinfo);
+    message.delete(10000)
+
+}
