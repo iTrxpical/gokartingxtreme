623806c54ef15806710aa553289c9b7439616af0
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-24 10:15:34 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (Discord, client, message, args) => {
 
-    message.delete(50)
+    message.delete(100)
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     var embedping = new Discord.RichEmbed()
         .setColor(randomColor)
