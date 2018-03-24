2df890bcc5f3dcd713d242bd8a31217550326dc9
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-24 20:02:03 +0000

    Create 1k.js

---
+++ commands/1k.js
@@ -0,0 +1,15 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+exports.run = (Discord, client, message, args) => {
+
+    var embedinfo = new Discord.RichEmbed()
+        .setFooter("© Go-Karting Xtreme | A group with 1000 members!")
+        .setColor(randomColor)
+        .setTitle(":100:  :sparkler: **__1000 MEMBERS__** :sparkler: :100:")
+        .setDescription("Woop, woop! We’ve hit 1000 members on our Roblox group. This is a massive achievement for us, and we want to thank all of our racers and staff for supporting us! We’ll be hosting a huge party on Monday 26th March to celebrate this. Details of the party will be released shortly, it will include a giveaway of R$450!")
+        .addField("Details Of Party:"," ")
+        .addField("Date","26/3/18")
+        .addField("Time","7PM-8:45PM (BST)")
+    message.channel.sendEmbed(embedinfo);
+
+}
