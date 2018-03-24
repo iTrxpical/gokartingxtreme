9dda493e729839b098b925e47b186dbfba14146c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-24 15:05:19 +0000

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,10 @@
+message.author.send("See or Change?");
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content === "See") {
+                message.channel.send("You Want To See Someones Spec OK!");
+            } else if (message.content === "Change") {
+                message.channel.send("You Want To Change Your Spec OK!");
+            }
+        })
