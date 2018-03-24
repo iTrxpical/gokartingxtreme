949b34d74c463560c01fd3690e7d9e2423c51ac7
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-24 20:16:37 +0000

    Update pic.js

--- commands/pic.js
+++ commands/pic.js
@@ -1,13 +1,4 @@
-var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 exports.run = (Discord, client, message, args) => {
-
-var embederrordm = new Discord.RichEmbed()
-        .setTitle("It's party time!:")
-        .setDescription("You're invited to our 1K member party!")
-        .setImage('https://cdn.discordapp.com/attachments/425907562039345155/426454936776081408/unknown.png')
-        .setFooter("Go-Karting Xtreme | 1K Member Party")
-        .setColor('#000000');
-        message.channel.send(embederrordm);
-        return;
+        message.reply("@everyone")
     }
