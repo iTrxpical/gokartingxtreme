0ababdc56233c988bc92bc1ce606702147d3561f
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-25 13:47:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ client.on('message', message => {
        let allowedRoleTwo = message.guild.roles.find("name", "Chief Executive Officer");
 
        if (message.channel.id === '422091691604967425') {
-       if(message.member.roles.has(allowedRole.id) {
+       if(message.member.roles.has(allowedRole.id)) {
          message.send("Message Allowed :ballot_box_with_check:")
-       } else if(message.member.roles.has(allowedRoleTwo.id) {
+       } else if(message.member.roles.has(allowedRoleTwo.id)) {
          message.send("Message Allowed :ballot_box_with_check:")
        } else if (sender.bot) {
          message.delete(5000)
