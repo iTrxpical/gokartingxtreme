43e840ca4366ba413d1b1b52d76cf6b8dd5c3b10
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-25 13:57:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ client.on('message', message => {
 
        if (message.channel.id === '422091691604967425') {
        if(message.member.roles.has(allowedRole.id)) {
-         message.send("Message Allowed :ballot_box_with_check:")
+         message.channel.send("Message Allowed :ballot_box_with_check:")
        } else if(message.member.roles.has(allowedRoleTwo.id)) {
-         message.send("Message Allowed :ballot_box_with_check:")
+         message.channel.send("Message Allowed :ballot_box_with_check:")
        } else if (sender.bot) {
          message.delete(5000)
        } else {
