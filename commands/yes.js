f0e28230ec1d1145bb1511b0f97bc95940cebdef
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-25 19:47:58 +0100

    Create yes.js

---
+++ commands/yes.js
@@ -0,0 +1,9 @@
+exports.run = (Discord, client, message, args) => {
+
+    message.delete(100)
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    var embedping = new Discord.RichEmbed()
+        .setColor(randomColor)
+        .addField("Yes!", "I can't wait.")
+    message.channel.send(embedping)
+}
