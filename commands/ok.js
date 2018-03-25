6003cf5436a977cdcdb39edaf2918e00ae2a6ad7
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-25 21:24:23 +0100

    Update ok.js

--- commands/ok.js
+++ commands/ok.js
@@ -6,7 +6,7 @@ exports.run = (Discord, client, message, args) => {
         .setColor(randomColor)
         .setTitle('Priavate Bookings')
         //.setImage('')
-        .setDescription('As of 25/3/18 you'll be able to book private sessions! This will mean that you can hire us for your own events. Below are our private booking plans. To book us, please create a ticket in #create-ticket.'
+        .setDescription('As of 25/3/18 you'll be able to book private sessions! This will mean that you can hire us for your own events. Below are our private booking plans. To book us, please create a ticket in #create-ticket.')
         //.addField("Yes!", "I can't wait.")
     message.channel.send(embedping);
     //message.channel.send("Me!")
