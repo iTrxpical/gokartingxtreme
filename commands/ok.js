91d8ae8b8766f9372874f9cfba65471f7139335e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-25 19:57:10 +0100

    Update ok.js

--- commands/ok.js
+++ commands/ok.js
@@ -2,8 +2,11 @@ exports.run = (Discord, client, message, args) => {
 
     message.delete(100)
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    //var embedping = new Discord.RichEmbed()
-        //.setColor(randomColor)
-       // .addField("Yes!", "I can't wait.")
-    message.channel.send("Please react below with :ballot_box_with_check: if you are attending!")
+    var embedping = new Discord.RichEmbed()
+        .setColor(randomColor)
+        .setTitle('#HYPE')
+        .setImage('https://lh3.googleusercontent.com/vwnUTKfCtde45bTBY2WnRHYt79If3uA1jLCIhYgNcFGYt_6wBrrP5ooHA-ZTq4WeyaPXd710ymhds4KJFKRw=w1680-h913')
+        //.addField("Yes!", "I can't wait.")
+    message.channel(embedping)
+    //message.channel.send("Please react below with :ballot_box_with_check: if you are attending!")
 }
