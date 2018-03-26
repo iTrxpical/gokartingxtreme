e94f14174bf625852cb867fb51c70fd84aa0acc8
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-26 20:56:50 +0100

    Update lol.js

--- commands/lol.js
+++ commands/lol.js
@@ -5,5 +5,5 @@ exports.run = (Discord, client, message, args) => {
     var embedping = new Discord.RichEmbed()
         .setColor(randomColor)
         .addField("Error 404: Unavalible","AaronnSmh and danfrostythesnowman3 are currently offline, if you require further assistance please call our helpline at 999 - Thanks, dan and aaron")
-    message.channel.send(embedping)
+    message.channel.send("@Keane709#4030 @tomsIy#0138)
   }
