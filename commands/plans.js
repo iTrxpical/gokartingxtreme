247f45c8fa6fbe10b172077b56b228f2f3d0dc81
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-27 20:55:05 +0100

    Update and rename bookinfotest.js to plans.js

--- commands/plans.js
+++ commands/plans.js
@@ -2,14 +2,14 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 exports.run = (Discord, client, message, args) => {
 
-    message.reply("Go-Karting Xtreme - Private Session Booking Info");
+    message.reply("Check your DMs for plan details!");
     var embedinfo = new Discord.RichEmbed()
   .setTitle("Go-Karting Xtreme")
   .setColor("#15f153")
   .setDescription("Private Booking Information & ToS")
   .addField("Payment", "Payment will be paid after our team have decided that the session will be able to go ahead. Once paid, no refunds will be given. We DM you on discord to discuss the final stages of private hire sessions.")
   .addField("Plans", "We have three stages for private hire sessions, `bronze`, `silver` and `gold`.")
-    message.channel.sendEmbed(embedinfo);
+    message.author.sendEmbed(embedinfo);
     //.then(m=>m.delete(10000))
     var embedgold = new Discord.RichEmbed()
   .setTitle("Gold") //Wait a sec I've got an idea, we should have it so different pages has different plans, let me find dat code.
@@ -20,7 +20,7 @@ exports.run = (Discord, client, message, args) => {
     .addField("Staff", "At least 8 working staff members")
     .addField("Race Types", "Free time, races and showdown avalible")
     .addField("Price", "R$400 (SALE)")
-    message.channel.sendEmbed(embedgold);
+    message.author.sendEmbed(embedgold);
     var embedsilver = new Discord.RichEmbed()
   .setTitle("Silver") //Wait a sec I've got an idea, we should have it so different pages has different plans, let me find dat code.
     .setColor("#C0C0C0")
@@ -30,7 +30,7 @@ exports.run = (Discord, client, message, args) => {
     .addField("Staff", "At least 6 working staff members")
     .addField("Race Types", "Free time, races and showdown avalible")
     .addField("Price", "R$250")
-    message.channel.sendEmbed(embedsilver);
+    message.author.sendEmbed(embedsilver);
     var embedbronze= new Discord.RichEmbed()
   .setTitle("Bronze") //Wait a sec I've got an idea, we should have it so different pages has different plans, let me find dat code.
     .setColor("#C0C0C0")
@@ -40,6 +40,6 @@ exports.run = (Discord, client, message, args) => {
     .addField("Staff", "At least 4 working staff members")
     .addField("Race Types", "Free time, races avalible")
     .addField("Price", "R$150")
-    message.channel.sendEmbed(embedbronze);
+    message.author.sendEmbed(embedbronze);
 
 }
