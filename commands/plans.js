8fffbc966099ab58cb1f982a2abdfd0a1edf3b12
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-27 20:57:01 +0100

    Update plans.js

--- commands/plans.js
+++ commands/plans.js
@@ -9,7 +9,7 @@ exports.run = (Discord, client, message, args) => {
   .setDescription("Private Booking Information & ToS")
   .addField("Payment", "Payment will be paid after our team have decided that the session will be able to go ahead. Once paid, no refunds will be given. We DM you on discord to discuss the final stages of private hire sessions.")
   .addField("Plans", "We have three stages for private hire sessions, `bronze`, `silver` and `gold`.")
-    message.author.sendEmbed(embedinfo);
+    message.channel.sendEmbed(embedinfo);
     //.then(m=>m.delete(10000))
     var embedgold = new Discord.RichEmbed()
   .setTitle("Gold") //Wait a sec I've got an idea, we should have it so different pages has different plans, let me find dat code.
@@ -20,7 +20,7 @@ exports.run = (Discord, client, message, args) => {
     .addField("Staff", "At least 8 working staff members")
     .addField("Race Types", "Free time, races and showdown avalible")
     .addField("Price", "R$400 (SALE)")
-    message.author.sendEmbed(embedgold);
+    message.channel.sendEmbed(embedgold);
     var embedsilver = new Discord.RichEmbed()
   .setTitle("Silver") //Wait a sec I've got an idea, we should have it so different pages has different plans, let me find dat code.
     .setColor("#C0C0C0")
@@ -30,7 +30,7 @@ exports.run = (Discord, client, message, args) => {
     .addField("Staff", "At least 6 working staff members")
     .addField("Race Types", "Free time, races and showdown avalible")
     .addField("Price", "R$250")
-    message.author.sendEmbed(embedsilver);
+    message.channel.sendEmbed(embedsilver);
     var embedbronze= new Discord.RichEmbed()
   .setTitle("Bronze") //Wait a sec I've got an idea, we should have it so different pages has different plans, let me find dat code.
     .setColor("#C0C0C0")
@@ -40,6 +40,6 @@ exports.run = (Discord, client, message, args) => {
     .addField("Staff", "At least 4 working staff members")
     .addField("Race Types", "Free time, races avalible")
     .addField("Price", "R$150")
-    message.author.sendEmbed(embedbronze);
+    message.channel.sendEmbed(embedbronze);
 
 }
