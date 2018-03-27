e84b9905f5dd737aca386ca827df1b869b06b7f6
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-27 17:19:53 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,8 +3,6 @@ exports.run = (Discord, client, message, args) => {
 
 let sender = message.author;
 let allowedRole = message.guild.roles.find("name", "Board Of Executives");
-let allowedRoleTwo = message.guild.roles.find("name", "Chief Executive Officer");
-
 
        if(message.member.roles.has(allowedRole.id)) {
          message.channel.send("Test")
