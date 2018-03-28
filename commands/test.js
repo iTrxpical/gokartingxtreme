3041d82fe005024a242c389ce1d0f88af8fb1452
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-28 16:28:06 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -5,7 +5,16 @@ let sender = message.author;
 let allowedRole = message.guild.roles.find("name", "Board Of Executives");
 
        if(message.member.roles.has(allowedRole.id)) {
-         message.channel.send("Test")
+        message.author.send("What plan would you like? Options are: `Gold` and `Silver`");
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        console.log(collector)
+        collector.on('collect', message => {
+            if (message.content === "Gold") {
+                message.channel.send("Your booking for `gold` session. What time and date would you like it at?");
+            } else if (message.content === "Silver") {
+                message.channel.send("Your booking for `silver` session. What time and date would you like it at?");
+            }
+        })
        } else {
          message.channel.send(":gear: Our furious workers (~~TheReal~~) are working hard to make something, don't try again please!")
          message.delete()
