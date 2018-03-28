ad535e14bb427774508acc7a70be2765e6fdf47c
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-28 19:34:49 +0100

    Update book.js

--- commands/book.js
+++ commands/book.js
@@ -1,5 +1,6 @@
 exports.run = (Discord, client, message, args) => {
 
+var type = 'NonDefined';
 const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
            message.channel.send("What plan do you wish to book?");
 	        collector.on('collect', message => {
