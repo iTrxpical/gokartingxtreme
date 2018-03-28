f224111cd11edadbd9b1d8fd9037879363a82a05
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-28 17:59:17 +0100

    Update ok.js

--- commands/ok.js
+++ commands/ok.js
@@ -4,10 +4,7 @@ exports.run = (Discord, client, message, args) => {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     var embedping = new Discord.RichEmbed()
         .setColor(randomColor)
-        .setTitle('Priavate Bookings')
-        //.setImage('')
-        .setDescription(' .')
-        //.addField("Yes!", "I can't wait.")
-    //message.channel.send(embedping);
-    //message.channel.send("**__Private Booking:__** As 25/3/18 you'll be able to book private sessions! This will mean that you can hire us for your own events. Below are our private booking plans. To book us, please create a ticket in #create-ticket.")
-}
+        .addField("Dan has now been muted :D", "He can't now talk.")
+    message.channel.send(embedping)
+    .then(m=>m.delete(10000))
+  }
