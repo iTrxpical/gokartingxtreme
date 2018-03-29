7a8c539c2d66bce0d7bf6f31997c156247662a77
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-29 14:38:32 +0100

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,8 @@
+exports.run = (Discord, client, message, args) => {
+
+const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+    }
