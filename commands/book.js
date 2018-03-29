f1c50abd07f14c7d4ba2811291434d9f51d5930d
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-29 19:20:47 +0100

    Update book.js

--- commands/book.js
+++ commands/book.js
@@ -12,7 +12,7 @@ var type = " ";
 var time = " ";
 
 const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 180000 });
-        message.channel.send("The max time for this booking process is `5 minutes`, if you take longer then it may not go through the system. **Notice: Our booking process will not tell you when the time is over, if you do not recieve a DM from our bot within an hour of booking, retry or contact our BoX team.**");
+        message.channel.send("The max time for this booking process is `3 minutes`, if you take longer then it may not go through the system. **Notice: Our booking process will not tell you when the time is over, if you do not recieve a DM from our bot within an hour of booking, retry or contact our BoX team.**");
 	message.channel.send("What plan do you wish to book? (`Gold`, `Silver` or `Bronze`)");
 	        collector.on('collect', message => {
             if (message.content == "Gold") {
