c95ea98252097683d7cfa8055f4a0bde9a7bdb20
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-29 15:58:11 +0100

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -4,7 +4,7 @@ const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
-    message.channel.send("@test");
+    message.channel.send("@everyone");
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     var embedping = new Discord.RichEmbed()
         .setColor(randomColor)
