eeea35a43194104afeffd9f5add6fce673195279
iTrxpical <34778563+iTrxpical@users.noreply.github.com>
2018-03-29 19:50:39 +0100

    Update book.js

--- commands/book.js
+++ commands/book.js
@@ -88,8 +88,8 @@ function embedBook() {
     bookingchannel.send(bookEmbed);
     message.reply("Your booking has been placed! Expect a DM from our team shortly!");
     message.author.send("Here is a recepit of your booking!");
-    message.reply(embedBook);
-    .then(m=>m.delete(10000))
+    message.reply(embedBook)
+    .then(m=>m.delete(10000));
 }
 setTimeout(embedBook,2e4)
 }
