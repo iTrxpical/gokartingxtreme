21843e361325059b0a72156dbb97e2c52cf6377c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-29 16:02:06 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,8 +1,19 @@
+
 exports.run = (Discord, client, message, args) => {
 
-const sayMessage = args.join(" ");
+let sender = message.author;
+let allowedRole = message.guild.roles.find("name", "Board Of Executives");
+
+       if(message.member.roles.has(allowedRole.id)) {
+        const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
-    }
+        
+       } else {
+         message.channel.send("This command is limited to BOX only.")
+         message.delete()
+         .then(m=>m.delete(10000))
+}}
+}
