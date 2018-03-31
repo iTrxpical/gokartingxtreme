91d5308a5891f6f703b59150d7b086c233a5d5a5
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-31 08:54:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,18 +15,24 @@ client.on('ready', () => {
 });
 
 
-client.on('message', message => {   
-   
+client.on('message', message => {    
+    
        let sender = message.author;
-   
+       let allowedRole = message.guild.roles.find("name", "Board Of Executives");
+       let allowedRoleTwo = message.guild.roles.find("name", "Chief Executive Officer");
+
        if (message.channel.id === '422091691604967425') {
-       if (sender.bot) {
+       if(message.member.roles.has(allowedRole.id)) {
+         message.channel.send("Message Allowed :ballot_box_with_check:")
+       } else if(message.member.roles.has(allowedRoleTwo.id)) {
+         message.channel.send("Message Allowed :ballot_box_with_check:")
+       } else if (sender.bot) {
          message.delete(5000)
        } else {
          message.delete()
        }}
     
-    if (sender.bot) return;
+if (sender.bot) return;
    
     if (message.channel.type === 'dm') {
         var embederrordm = new Discord.RichEmbed()
