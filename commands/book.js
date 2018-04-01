6b9ddbc8896af5c90b36785b77f4e5a1d24b03a8
danfrostythesnowman1 <da_brown@outlook.com>
2018-04-01 09:37:29 +0100

    Update book.js

--- commands/book.js
+++ commands/book.js
@@ -38,9 +38,12 @@ const collector = new Discord.MessageCollector(message.channel, m => m.author.id
 		message.channel.send(embedbronze)
 		type = 'Bronze'
 	    }
-		const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 60000 });
-           message.channel.send("What time and date do you with to book for?");
+	
 		})
+var time = "";
+	
+const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 100 });
+           message.channel.send("What time and date do you with to book for?");
 
         
 			     
@@ -53,6 +56,7 @@ function embedBook() {
     .setColor("#15f153")
     .addField("Booker", `${bookname} with the ID: ${bookname.id}`)
     .addField("Type", `${type}`)
+    .addField("Time", `${time}`)
     .addField("Booked At", message.createdAt)
     .setFooter("Private Booking V1.0");
 
